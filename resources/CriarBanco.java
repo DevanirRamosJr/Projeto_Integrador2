9df488d3c84a651fab35d1b4adf006bbec982637
TairikJohnny <tairikjohnny@gmail.com>
2020-11-05 11:11:39 -0300

    Adicionando os arquivos CriarBanco.java, CriarTabelaAgua.java, CriarTabelaLuz.java, FabricaConexao.java e TestaConexao.java

---
+++ resources/CriarBanco.java
@@ -0,0 +1,33 @@
+package source;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CriarBanco {
+	
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		
+		final String url = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
+		final String usuario = "root";
+		final String senha = "123456789";
+		
+		// CRIANDO CONEXAO
+		Connection conexao = DriverManager.getConnection(url, usuario, senha);
+		
+		Statement stmt = conexao.createStatement();
+		
+		// CRIANDO BANCO DE DADOS SE ELE NÃO EXISTIR
+		stmt.execute("CREATE DATABASE IF NOT EXISTS projeto_integrador");
+		
+		// DELETANDO O BANCO CASO ELE EXISTA
+		//stmt.execute("DROP DATABASE IF EXISTS curso_java");
+		
+		System.out.println("Banco criado com sucesso!!!");
+		
+		conexao.close();
+	}
+
+}
