042e7a42f4c9e047ee4924feb236a74e0be67276
Devanir <61068102+De472@users.noreply.github.com>
2020-11-05 23:37:20 -0300

    login adicionado

--- resources/TelaInicial.java
+++ resources/TelaInicial.java
@@ -194,7 +194,7 @@ public class TelaInicial extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaInicial().setVisible(true);
+                new TelaLogin().setVisible(true);
             }
         });
     }
