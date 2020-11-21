5e44fab89ea64796c4ef5560d4c20d8ed492168e
Devanir <61068102+De472@users.noreply.github.com>
2020-11-21 17:47:44 -0300

    cria valores para as tabelas cliente e local

--- resources/CriarTabelas.java
+++ resources/CriarTabelas.java
@@ -10,10 +10,10 @@ public class CriarTabelas {
 	public static void main() throws SQLException {
 		cliente();
 		local();
-		conta_agua();
 		ref_agua();
-		conta_luz();
+		conta_agua();
 		ref_luz();
+		conta_luz();
 		login();
 		}
 	
@@ -24,14 +24,23 @@ public class CriarTabelas {
 		String sql = "CREATE TABLE IF NOT EXISTS cliente ("
 				+ "id_cli int PRIMARY KEY NOT NULL AUTO_INCREMENT,"
 				+ "nome_cli VARCHAR(50) NOT NULL,"
-				+ "cnpj_cli CHAR(11) NOT NULL,"
+				+ "cnpj_cli CHAR(18) NOT NULL,"
 				+ "telefone_cli VARCHAR(20) NOT NULL"
 				+ ")";
-		
 		Statement stmt = conexao.createStatement();
 		stmt.execute(sql);
-		
 		System.out.println("Cliente criada com sucesso!!!");
+		
+		PreparedStatement posted = conexao.prepareStatement("INSERT IGNORE INTO cliente (nome_cli, cnpj_cli, telefone_cli) VALUES"
+				+ "('Loja Variedades', '11.222.333/0001-11', '1199123456'),"
+				+ "('Livraria Papel Novo', '22.222.555/0010-10', '11997894567'),"
+				+ "('Mercadinho do José', '12.099.123/0004-06', '12992428534'),"
+				+ "('Utensilhos Já', '25.551.946/0880-06', '11992922277'),"
+				+ "('Leve Mais Poupe Mais', '20.202.587/0590-19', '11997585090'),"
+				+ "('Loucos por Tech', '12.292.525/202-20', '11998755494')");
+		posted.executeUpdate();
+		
+		System.out.println("Cliente - dados criados com sucesso!!!");
 		conexao.close();
 	}
 	
@@ -43,18 +52,27 @@ public class CriarTabelas {
 				+ "id_loc int PRIMARY KEY NOT NULL AUTO_INCREMENT,"
 				+ "id_cli int NOT NULL,"
 				+ "endereco VARCHAR(70) NOT NULL,"
-				+ "complemento VARCHAR(100) NOT NULL,"
+				+ "complemento VARCHAR(100),"
 				+ "cep VARCHAR(20) NOT NULL,"
 				+ "cidade VARCHAR(40) NOT NULL,"
 				+ "uf CHAR(2) NOT NULL,"
-				+ "pais VARCHAR (40) NOT NULL,"
+				+ "pais CHAR (2) NOT NULL,"
 				+ "FOREIGN KEY (id_cli) REFERENCES cliente(id_cli)"
 				+ ")";
 		
 		Statement stmt = conexao.createStatement();
 		stmt.execute(sql);
-		
 		System.out.println("Local criada com sucesso!!!");
+		
+		PreparedStatement posted = conexao.prepareStatement("INSERT IGNORE INTO local (id_cli, endereco, complemento, cep, cidade, uf, pais) VALUES"
+				+ "('1', 'Rua Para', 'Bairro Jardim Alvorada', '12289367', 'Caçapava', 'SP', 'BR'),"
+				+ "('2', 'Rua Maranhao', 'Bairro Jardim Alvorada', '12289368', 'Caçapava', 'SP', 'BR'),"
+				+ "('3', 'Avenida Brasil', 'Centro', '25479952', 'São Paulo', 'SP', 'BR'),"
+				+ "('4', 'Avenida Paulista', 'Centro', '59621485', 'São Paulo', 'SP', 'BR'),"
+				+ "('5', 'Rua São João', 'Atrás do apartamento Unity', '15948632', 'Taubate', 'SP', 'BR'),"
+				+ "('6', 'Rua Coronel Manuel', '', '98745635', 'São José', 'SP', 'BR')");
+		posted.executeUpdate();
+		System.out.println("Local - dados criados com sucesso!!!");
 		conexao.close();
 	}
 	
@@ -151,9 +169,9 @@ public class CriarTabelas {
 		
 		System.out.println("Tabela 'login' criada com sucesso!!!");
 		
-		//jÃ¡ adiciona alguns logins
-		PreparedStatement posted = conexao.prepareStatement("INSERT IGNORE INTO login VALUES("
-				+ "'admin', 'password'),"
+		//já adiciona alguns logins
+		PreparedStatement posted = conexao.prepareStatement("INSERT IGNORE INTO login VALUES"
+				+ "('admin', 'password'),"
 				+ "('devanir', 'abcd1234')");
 		posted.executeUpdate();
 
